eadd8790262cb1856970964838b8385f89d5053e
mattdav00712 <mattd@cs.utexas.edu>
2015-07-03 21:33:39 -0500

    timming stuff in AES.java

--- project4/AES.java
+++ project4/AES.java
@@ -6,6 +6,7 @@ import java.util.regex.PatternSyntaxException;
 public class AES {
 	
 	public static void main(String[] args) throws FileNotFoundException {
+		long startTime = System.currentTimeMillis();
 		char option = args[0].charAt(0);
 		String keyFileName = args[1];
 		String inputFileName = args[2];
@@ -16,7 +17,14 @@ public class AES {
 		else if (option == 'd' || option == 'D') {
 			Decryptor dec = new Decryptor(keyFileName, inputFileName);
 		} else throw new IllegalArgumentException("[option] must be 'e' or 'd'");
-		// String t=hexToBinary("f2");
-		// System.out.println(t);
+
+		
+		File inputFile=new File(inputFileName);;
+		long endTime   = System.currentTimeMillis();
+		long totalTime = (endTime - startTime)/1000;
+		System.out.println("totalTime: "+totalTime+"sec");
+		
+		long bandwidth=(inputFile.length()*8)/totalTime;
+		System.out.println("Bandwidth: "+bandwidth+ "mb/s");
 	}
 }
